e99765bc671fd08e85aff1fa526912550dedcfbf
Manuel Schnidrig <el.manu@gmail.com>
2017-07-18 10:26:50 -0300

    Unit Test for operation added

---
+++ lib/scraper/test_operations.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from operations import CountNumberOfElements
+
+class DomBuilderMock():
+    lastSuscribeCall = None
+    def subscribe(self, *args):
+        self.lastSuscribeCall = args
+
+class CountNumberOfElementsTest(TestCase):
+    def setUp(self):
+        self.instance = CountNumberOfElements()
+
+    def test_attachToSuscribeToParsingFinished(self):
+        domBuilderMock = DomBuilderMock() 
+        self.instance.attachTo(domBuilderMock)
+
+        self.assertEqual("ParsingFinished", domBuilderMock.lastSuscribeCall[0])
+
+    def test_resultIsCorrect(self):
+        testDom = [('body', None), ('div', 0), ('p', 1), ('p', 1)] 
+        self.instance._handleParsingFinished({'dom': testDom})
+
+        self.assertEqual(4, self.instance.getResults())
+
+        testDom = [] 
+        self.instance._handleParsingFinished({'dom': testDom})
+
+        self.assertEqual(0, self.instance.getResults())
+
+        testDom = [('body', None)] 
+        self.instance._handleParsingFinished({'dom': testDom})
+
+        self.assertEqual(1, self.instance.getResults())
+        
